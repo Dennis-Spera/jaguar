e6d20feac566b03bf80e86b6f87ad35822297741
kadmin <dennis.spera@gmail.com>
2025-03-07 09:30:59 -0500

    2025-03-07-09-30

--- upload.sh
+++ upload.sh
@@ -4,6 +4,6 @@ datestamp=$(date +"%Y-%m-%d-%H-%M")
 git add -A
 git branch -M main
 git commit -m $datestamp
-git remote add origin  https://github.com/Dennis-Spera/jaguar.git
+#git remote add origin  https://github.com/Dennis-Spera/jaguar.git
 git push -u origin master
 git push https://github.com/Dennis-Spera/jaguar.git

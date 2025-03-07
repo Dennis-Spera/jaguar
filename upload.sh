a458fd921a9608a5266f69daa2367fdd71fc5e25
kadmin <dennis.spera@gmail.com>
2025-03-07 10:00:44 -0500

    2025-03-07-10-00

--- upload.sh
+++ upload.sh
@@ -1,5 +1,20 @@
 datestamp=$(date +"%Y-%m-%d-%H-%M")
 
+#kadmin@jupiter:~/Dropbox/projects/jaquar$ bash upload.sh
+#[detached HEAD c23aa65] 2025-03-07-09-54
+# 1 file changed, 1 insertion(+)
+#To https://github.com/Dennis-Spera/jaguar.git
+# ! [rejected]        HEAD -> main (fetch first)
+#error: failed to push some refs to 'https://github.com/Dennis-Spera/jaguar.git'
+#hint: Updates were rejected because the remote contains work that you do not
+#hint: have locally. This is usually caused by another repository pushing to
+#hint: the same ref. If you want to integrate the remote changes, use
+#hint: 'git pull' before pushing again.
+#hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+
+# -> solution below
+#kadmin@jupiter:~/Dropbox/projects/jaquar$ git pull https://github.com/Dennis-Spera/jaguar.git HEAD:main
+
 
 git add -A
 #git branch -M main

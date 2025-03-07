82b79839f2dfbb8fed9f72e787352f9edcfd40de
Dennis-Spera <dennis.spera@gmail.com>
2025-03-07 09:35:42 -0500

    Delete upload.sh

--- upload.sh
+++
@@ -1,9 +0,0 @@
-datestamp=$(date +"%Y-%m-%d-%H-%M")
-
-
-git add -A
-git branch -M main
-git commit -m $datestamp
-#git remote add origin  https://github.com/Dennis-Spera/jaguar.git
-#git push -u or
-git push https://github.com/Dennis-Spera/jaguar.git HEAD:main

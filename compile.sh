7815c69b35879e761cbd5063636fde758bb39481
kadmin <dennis.spera@gmail.com>
2025-03-07 09:43:17 -0500

    2025-03-07-09-43

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 export files="appName.py bytesRead.py collScans.py cpuc.py \
               docsExamined.py drivers.py formatOne.py jsonFetcher.py keysExamined.py millis.py \
               ns.py nsCount.py nsCountUser.py nsIndexes.py phead.py prettyJson.py queryHash.py"

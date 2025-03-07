cf118c8f9be0c986bd189b3c636b122a652faa31
kadmin <dennis.spera@gmail.com>
2025-03-07 09:54:00 -0500

    2025-03-07-09-54

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ export files="appName.py bytesRead.py collScans.py cpuc.py \
               docsExamined.py drivers.py formatOne.py jsonFetcher.py keysExamined.py millis.py \
               ns.py nsCount.py nsCountUser.py nsIndexes.py phead.py prettyJson.py queryHash.py"
 
+
 for file in $files; do
     pyinstaller --onefile $file
 done

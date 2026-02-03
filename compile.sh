59c59424b0c3c17ac88edbe395c309008cef46c0
kadmin <dennis.spera@gmail.com>
2026-02-03 10:26:18 -0500

    add core 2-03-2026-1

--- compile.sh
+++ compile.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export files="appName.py bytesRead.py collScans.py cpuc.py \
-              docsExamined.py drivers.py formatOne.py jsonFetcher.py keysExamined.py millis.py \
-              ns.py nsCount.py nsCountUser.py nsIndexes.py phead.py prettyJson.py queryHash.py"
-
-for file in $files; do
+for file in *.py; do
     pyinstaller --onefile $file
 done
